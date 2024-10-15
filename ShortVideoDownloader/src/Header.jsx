f01217e370f5a9ffc96aa5eadd1500840ef8be00
Balram <22bce224@nirmauni.ac.in>
2024-10-15 14:02:14 +0530

    uploaded project

---
+++ ShortVideoDownloader/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div className="header">
+      <header>
+        <h1>This heading 1</h1>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>More Tools</li>
+          <li>Help</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
